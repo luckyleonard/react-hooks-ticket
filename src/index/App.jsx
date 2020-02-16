d942618a64411111cf41f3cdefccaf2cc2c8aa5e
luckyleonard <dzhu31@hotmail.com>
2020-02-16 18:12:00 +1100

    📦 feat: build Component structure for index
    - with five Component
    * Common/Header
    * DepartDate
    * Journey
    * HighSpeed
    * Submit

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import Header from '../common/Header';
+import DepartDate from './DepartDate';
+import HighSpeed from './HighSpeed';
+import Journey from './Journey';
+import Submit from './Submit';
 
-function App(props) {}
+function App(props) {
+  return (
+    <div>
+      <Header></Header>
+      <DepartDate></DepartDate>
+      <HighSpeed></HighSpeed>
+      <Journey></Journey>
+      <Submit></Submit>
+    </div>
+  );
+}
 
 export default connect(
   function mapStateToProps(state) {},
