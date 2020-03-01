a6dd31aee2db4d1e61e7f5a1408b80bc2ccd9a65
luckyleonard <dzhu31@hotmail.com>
2020-03-02 01:46:46 +1100

    ✨ feat(query): Build the UI structure of Query page

---
+++ src/common/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Nav.css';
+
+export default function Nav(props) {
+  return null;
+}
+
+Nav.propTypes = {};
--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Nav from '../common/Nav';
+import List from './List';
+import Bottom from './Bottom';
 import './App.css';
 
-function App(props) {}
+function App(props) {
+  return (
+    <>
+      <Nav />
+      <List />
+      <Bottom />
+    </>
+  );
+}
 
 export default connect(
-  function mapStateToProps(state) {},
-  function mapDispatchToProps(dispatch) {}
+  function mapStateToProps(state) {
+    return state;
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch };
+  }
 )(App);
