9ccc722593274b16f9f19c61cb78671280197116
luckyleonard <dzhu31@hotmail.com>
2020-03-26 10:52:27 +1100

    ✨ feat(Nav) Introduce Nav Component for query the ticket
    - encapsulation a useNav hooks to process the data that will use in the Nav
    * to decouple the data process logic with App

--- src/common/Nav.jsx
+++ src/common/Nav.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import dayjs from 'dayjs';
 import './Nav.css';
 
-export default function Nav(props) {
-  return null;
-}
+const Nav = memo(function Nav(props) {
+  const { date, prev, next, isPrevDisabled, isNextDisabled } = props;
 
-Nav.propTypes = {};
+  const currentString = useMemo(() => {
+    const d = dayjs(date);
+    return d.format('DD/MM');
+  }, [date]);
+
+  return (
+    <div className='nav'>
+      <span
+        onClick={prev}
+        className={classnames('nav-prev', {
+          'nav-disabled': isPrevDisabled
+        })}>
+        Last Day
+      </span>
+      <span
+        onClick={next}
+        className={classnames('nav-next', {
+          'nav-disabled': isNextDisabled
+        })}>
+        Next Day
+      </span>
+      <span className='nav-date'>{currentString}</span>
+    </div>
+  );
+});
+
+Nav.propTypes = {
+  date: PropTypes.number.isRequired,
+  prev: PropTypes.func.isRequired,
+  next: PropTypes.func.isRequired,
+  isPrevDisabled: PropTypes.bool.isRequired,
+  isNextDisabled: PropTypes.bool.isRequired
+};
+
+export default Nav;
---
+++ src/common/useNav.jsx
@@ -0,0 +1,25 @@
+import { formatDate } from './dateFormater';
+import { useCallback } from 'react';
+
+export default function useNav(departDate, dispatch, prevDate, nextDate) {
+  const isPrevDisabled = formatDate(departDate) <= formatDate();
+
+  const isNextDisabled =
+    formatDate(departDate) - formatDate() > 20 * 86400 * 1000;
+
+  const prev = useCallback(() => {
+    if (isPrevDisabled) {
+      return;
+    }
+    dispatch(prevDate());
+  }, [isPrevDisabled]);
+
+  const next = useCallback(() => {
+    if (isNextDisabled) {
+      return;
+    }
+    dispatch(nextDate());
+  }, [isNextDisabled]);
+
+  return { isPrevDisabled, isNextDisabled, prev, next };
+}
