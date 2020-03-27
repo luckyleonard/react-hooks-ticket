b2b5d19e3a29c3e4b93f1a0bc533af2506220812
luckyleonard <dzhu31@hotmail.com>
2020-03-27 16:44:14 +1100

    ✨ feat(List) build a List component to show the ticket info
    - get data from App with fetch
    - pass the query to ticket.html

--- src/query/List.jsx
+++ src/query/List.jsx
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import URI from 'urijs';
 import './List.css';
 
-export default function List(props) {
-  return null;
-}
+const ListItem = memo(function ListItem(props) {
+  const {
+    dTime,
+    aTime,
+    dStation,
+    aStation,
+    trainNumber,
+    date,
+    time,
+    priceMsg,
+    dayAfter
+  } = props;
 
-List.propTypes = {};
+  const url = useMemo(() => {
+    return new URI('ticket.html')
+      .setSearch('aStation', aStation)
+      .setSearch('dStation', dStation)
+      .setSearch('trainNumber', trainNumber)
+      .setSearch('date', date)
+      .toString();
+  }, [aStation, dStation, trainNumber, date]);
+
+  return (
+    <li className='list-item'>
+      <a href={url}>
+        <span className='item-time'>
+          <em>{dTime}</em>
+          <br />
+          <em className='em-light'>
+            {aTime}
+            <i className='time-after'>{dayAfter}</i>
+          </em>
+        </span>
+        <span className='item-stations'>
+          <em>
+            <i className='train-station train-start'>D</i>
+            {dStation}
+          </em>
+          <br />
+          <em className='em-light'>
+            <i className='train-station train-end'>A</i>
+            {aStation}
+          </em>
+        </span>
+        <span className='item-train'>
+          <em>{trainNumber}</em>
+          <br />
+          <em className='em-light'>{time}</em>
+        </span>
+        <span className='item-ticket'>
+          <em>{priceMsg}</em>
+          <br />
+          <em className='em-light-orange'>get it now</em>
+        </span>
+      </a>
+    </li>
+  );
+});
+ListItem.propTypes = {
+  dTime: PropTypes.string.isRequired,
+  aTime: PropTypes.string.isRequired,
+  dStation: PropTypes.string.isRequired,
+  aStation: PropTypes.string.isRequired,
+  trainNumber: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  priceMsg: PropTypes.string.isRequired,
+  dayAfter: PropTypes.string.isRequired
+};
+
+const List = memo(function List(props) {
+  const { list } = props;
+  return (
+    <ul className='list'>
+      {list.map(item => (
+        <ListItem {...item} key={item.trainNumber} />
+      ))}
+    </ul>
+  );
+});
+
+List.propTypes = {
+  list: PropTypes.array.isRequired
+};
+
+export default List;
