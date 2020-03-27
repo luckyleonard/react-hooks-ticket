a3018d7b696afb943654113d72684a4124d6e165
luckyleonard <dzhu31@hotmail.com>
2020-03-27 16:47:09 +1100

    ✨ feat(Bottom) build a Bottom component to show the filte
    - get data from App from redux store
    - state change with clicking on filters
    * fliters link to a complex filter layer

--- src/query/Bottom.jsx
+++ src/query/Bottom.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Bottom.css';
+import { ORDER_DEPART } from './constant';
+import classnames from 'classnames';
 
 export default function Bottom(props) {
-  return null;
+  const {
+    toggleOrderTypes,
+    toggleHighSpeed,
+    toggleOnlyTickets,
+    toggleIsFiltersVisible,
+    highSpeed,
+    orderTyps,
+    onlyTickets,
+    isFiltersVisible
+  } = props;
+  return (
+    <div className='bottom'>
+      <div className='bottom-filters'>
+        <span className='item' onClick={toggleOrderTypes}>
+          <i className='icon'>&#xf065;</i>
+          {orderTyps === ORDER_DEPART ? 'Depart Time' : 'Duration'}
+        </span>
+        <span
+          className={classnames('item', { 'item-on': highSpeed })}
+          onClick={toggleHighSpeed}>
+          <i className='icon'>{highSpeed ? '\uf43f' : '\uf43e'}</i>
+          Direct Fly
+        </span>
+        <span
+          className={classnames('item', { 'item-on': onlyTickets })}
+          onClick={toggleOnlyTickets}>
+          <i className='icon'>{onlyTickets ? '\uf43d' : '\uf43c'}</i>
+          Available
+        </span>
+        <span
+          className={classnames('item', { 'item-on': isFiltersVisible })}
+          onClick={toggleIsFiltersVisible}>
+          <i className='icon'>{'\uf0f7'}</i>
+          Filter
+        </span>
+      </div>
+    </div>
+  );
 }
 
-Bottom.propTypes = {};
+Bottom.propTypes = {
+  toggleOrderTypes: PropTypes.func.isRequired,
+  toggleHighSpeed: PropTypes.func.isRequired,
+  toggleOnlyTickets: PropTypes.func.isRequired,
+  toggleIsFiltersVisible: PropTypes.func.isRequired,
+  highSpeed: PropTypes.bool.isRequired,
+  orderTyps: PropTypes.number.isRequired,
+  onlyTickets: PropTypes.bool.isRequired,
+  isFiltersVisible: PropTypes.bool.isRequired
+};
