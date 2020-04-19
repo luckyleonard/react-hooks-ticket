b193d2aff4cd17c71e4e74b24d794855ef4c154a
luckyleonard <dzhu31@hotmail.com>
2020-04-19 22:13:35 +1000

    ✨ (App) introduce all props and initial Header and Detail
    - parsed URL to get data from URI
    - dispatch fetchInitial to get data from APIs

--- src/order/App.jsx
+++ src/order/App.jsx
@@ -1,10 +1,113 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { connect } from 'react-redux';
+import URI from 'urijs';
+import dayjs from 'dayjs';
+
+import Header from '../common/Header';
+import Detail from '../common/Detail';
+import Account from './Account';
+import Choose from './Choose';
+import Passengers from './Passengers';
+import Ticket from './Ticket';
 import './App.css';
 
-function App(props) {}
+import {
+  setDepartStation,
+  setArriveStation,
+  setTrainNumber,
+  setSeatType,
+  setDepartDate,
+  setSearchParsed,
+  fetchInitial,
+} from './store/actionCreator';
+
+function App(props) {
+  const {
+    trainNumber,
+    departStation,
+    arriveStation,
+    seatType,
+    departDate,
+    arriveDate,
+    departTimeStr,
+    arriveTimeStr,
+    durationStr,
+    price,
+    passengers,
+    menu,
+    isMenuVisible,
+    searchParsed,
+    dispatch,
+  } = props;
+
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search);
+    const { trainNumber, dStation, aStation, type, date } = queries;
+
+    dispatch(setDepartStation(dStation));
+    dispatch(setArriveStation(aStation));
+    dispatch(setTrainNumber(trainNumber));
+    dispatch(setSeatType(type));
+    dispatch(setDepartDate(dayjs(date).valueOf()));
+    dispatch(setSearchParsed(true));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!searchParsed) {
+      return;
+    }
+
+    const url = new URI('/rest/order')
+      .setSearch('dStation', departStation)
+      .setSearch('aStation', arriveStation)
+      .setSearch('type', seatType)
+      .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+      .toString();
+    dispatch(fetchInitial(url));
+  }, [
+    searchParsed,
+    departStation,
+    arriveStation,
+    seatType,
+    departDate,
+    dispatch,
+  ]);
+
+  if (!searchParsed) {
+    return null;
+  }
+
+  return (
+    <div className='app'>
+      <div className='header-wrapper'>
+        <Header title='Booking Inform' Back={onBack} />
+      </div>
+      <div className='detail-wrapper'>
+        <Detail
+          departDate={departDate}
+          arriveDate={arriveDate}
+          departTimeStr={departTimeStr}
+          arriveTimeStr={arriveTimeStr}
+          trainNumber={trainNumber}
+          departStation={departStation}
+          arriveStation={arriveStation}
+          durationStr={durationStr}>
+          <span style={{ display: 'block' }} className='train-icon' />
+        </Detail>
+      </div>
+    </div>
+  );
+}
 
 export default connect(
-  function mapStateToProps(state) {},
-  function mapDispatchToProps(dispatch) {}
+  function mapStateToProps(state) {
+    return state;
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch };
+  }
 )(App);
